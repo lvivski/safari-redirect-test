f1290e74b744999a3189b65fed49964bb84698e6
Yehor Lvivski <yehor@lvivski.com>
2024-09-05 01:56:36 +0000

    test

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log('script' + window.self !== window.top ? ' iframe' : '', window.location.search, window.location.hash);
+console.log('script' + (window.self !== window.top ? ' iframe' : ''), window.location.search, window.location.hash);
